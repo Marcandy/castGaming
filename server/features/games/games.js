b959a92d78c46d9a954cb801c98bd37add08e63f
marcandy <marcandy.nj@gmail.com>
2016-10-30 19:34:42 -0500

    user, databse

--- server/features/games/games.js
+++ server/features/games/games.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Game = new mongoose.Schema({
   name: {type:String},
   subtext: {type:String},
-  imageUrl: {type: String}
+  imgUrl: {type: String}
 })
 
 module.exports = mongoose.model('Game', Game);
